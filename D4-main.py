34b6bec6b30bb3fcd3d31ab3696c3f8aa9b387a4
Davide Raucci <davide.raucci.k@gmail.com>
2021-07-02 13:58:00 +0200

    Update D4-main.py

--- D4-main.py
+++ D4-main.py
@@ -1,2 +1,9 @@
 def strive_school():
+    x = input('Are you a Strive School student?\n').lower()
+    if x == 'yes':
     print("I'm gonna creating a conflict :D")
+    elif x == 'no':
+    print("I'm gonna creating a conflict :D")
+
+
+strive_school()
