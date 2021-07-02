f35c917a9733442df6e174c86ae82268107f24b6
Davide Raucci <davide.raucci.k@gmail.com>
2021-07-02 15:53:58 +0200

    Revert "Update D4-main.py"
    This reverts commit b618a136af737639f96f26c947e058ac6d5eee98.

--- D4-main.py
+++ D4-main.py
@@ -1,5 +1,5 @@
 def strive_school():
-x = input('Are you a Strive School student?\n').lower()
+    x = input('Are you a Strive School student?\n').lower()
     if x == 'yes':
     print("I'm gonna creating a conflict :D")
     elif x == 'no':
