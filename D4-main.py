49e84586af99ad02c866581a7a4e75898d9b2933
BirgitSmadja <86657210+BirgitSmadja@users.noreply.github.com>
2021-07-04 14:55:27 +0200

    Update D4-main.py
    4.7.21 first version comment added

--- D4-main.py
+++ D4-main.py
@@ -1,8 +1,7 @@
+# First version
 def strive_school():
     x = input('Are you a Strive School student?\n').lower()
     if x == 'yes':
         print("You're not getting enough sleep!")
     elif x == 'no':
         print("You're getting plenty of sleep!")
-
-strive_school()
